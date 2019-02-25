1179ccf7f507aa1605b3ce1567031bb242533d8a
Yanni App13 <jonvincruman@gmail.com>
2019-02-25 15:01:56 -0800

    removed unecessary code

--- Intervals.java
+++ Intervals.java
@@ -25,5 +25,46 @@ public class Intervals {
         // Prompt User
         System.out.println("Enter earlier start and end time as two " + 
             "24-hour format times.");
+
+        // First Time Interval
+        System.out.print("Enter earlier start and end time as two 24-hour format times: ");
+        firstIntervalStart = input.nextInt();
+        firstIntervalEnd = input.nextInt();
+
+        // Second Time Interval
+        System.out.print("Second time interval start: ");
+        secondIntervalStart = input.nextInt();
+        System.out.print("Second time interval end: ");
+        secondIntervalEnd = input.nextInt();
+
+        // Calculate First Time Interval
+        firstTotalTime = firstIntervalEnd - firstIntervalStart;
+        System.out.println("The earlier interval is " + firstTotalTime + 
+            " minutes long.");
+        
+        // Calculate Second Time Interval
+        secondTotalTime = secondIntervalEnd - secondIntervalStart;
+        System.out.println("The later interval is " + secondTotalTime + 
+            " minutes long.");
+        
+        // Compare total times print which interval is longer
+        if (firstTotalTime == secondTotalTime) {
+            System.out.println("Both intervals are the same duration");
+        }
+        else if(firstTotalTime > secondTotalTime) {
+            System.out.println("The first interval is longer");
+        }
+        else {
+            System.out.println("The second interval is longer");
+        }
+
+        // Determine if the intervals overlap
+        if (firstIntervalStart <= secondIntervalEnd 
+            && secondIntervalStart <= firstIntervalEnd) {
+            System.out.println("These intervals overlap.");
+        }
+        else {
+            System.out.println("These intervals do not overlap.");
+        }
     }
 }

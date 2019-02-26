d1920a47749bc9e3f41d6104711b923ba4733cdc
Yanni App13 <jonvincruman@gmail.com>
2019-02-25 16:09:11 -0800

    added algorith for converting military time to total minutes

--- Intervals.java
+++ Intervals.java
@@ -17,45 +17,101 @@ public class Intervals {
         // Declare variables
         int firstIntervalStart, firstIntervalEnd, secondIntervalStart;
         int secondIntervalEnd, firstTotalTime, secondTotalTime;
-        boolean does_overlap;
+        int firstStartHours, firstStartMinutes, firstStartTotalMinutes;
+        int firstEndHours, firstEndMinutes, firstEndTotalMinutes;
+        int secondStartHours, secondStartMinutes, secondStartTotalMinutes;
+        int secondEndHours, secondEndMinutes, secondEndTotalMinutes;
+        int firstStartHoursToMinutes, firstEndHoursToMinutes;
+        int secondStartHoursToMinutes, secondEndHoursToMinutes;
+        int temp; // Temp variable is declared to hold numbers for swap
         
         // Declare Scanner Object
         Scanner input = new Scanner(System.in);
 
-        // Prompt User
-        System.out.println("Enter earlier start and end time as two " + 
-            "24-hour format times.");
-
-        // First Time Interval
-        System.out.print("Enter earlier start and end time as two 24-hour format times: ");
+        // Prompt User to input First Time Interval
+        System.out.print("Enter earlier start and end time as two 24-hour "
+            + "format times: ");
         firstIntervalStart = input.nextInt();
         firstIntervalEnd = input.nextInt();
 
-        // Second Time Interval
-        System.out.print("Second time interval start: ");
+        // Validate first interval inputs and swap if necessary
+        if (firstIntervalStart > firstIntervalEnd) {
+            temp = firstIntervalStart;
+            firstIntervalStart = firstIntervalEnd;
+            firstIntervalEnd = temp;
+
+            // Debugging
+            System.out.println(firstIntervalStart);
+            System.out.println(firstIntervalEnd);
+        }
+
+        // Convert First Interval Start to Minutes
+        firstStartHours = firstIntervalStart / 100;
+        firstStartHoursToMinutes = firstStartHours * 60;
+        firstStartMinutes = firstIntervalStart % 100;
+        firstStartTotalMinutes = firstStartHoursToMinutes 
+            + firstStartMinutes;
+
+        // Convert First Interval End Minutes
+        firstEndHours = firstIntervalEnd / 100;
+        firstEndHoursToMinutes = firstEndHours * 60;
+        firstEndMinutes = firstIntervalEnd % 100;
+        firstEndTotalMinutes = firstEndHoursToMinutes + firstEndMinutes;
+
+        // First Interval Total Time in Minutes
+        firstTotalTime = firstEndTotalMinutes - firstStartTotalMinutes;
+
+        // Prompt User to InputSecond Time Interval
+        System.out.print("Enter later start and end time as two 24-hour "
+            + "format times: ");
         secondIntervalStart = input.nextInt();
-        System.out.print("Second time interval end: ");
         secondIntervalEnd = input.nextInt();
 
-        // Calculate First Time Interval
-        firstTotalTime = firstIntervalEnd - firstIntervalStart;
+        // Validate second interval inputs and swap if necessary
+        if (secondIntervalStart > secondIntervalEnd) {
+            temp = secondIntervalStart;
+            secondIntervalStart =  secondIntervalEnd;
+            secondIntervalEnd = temp;
+
+            // Debugging
+            System.out.println(secondIntervalStart);
+            System.out.println(secondIntervalEnd);
+
+        }
+
+        // Convert Second Time Start to Minutes
+        secondStartHours = secondIntervalStart / 100;
+        secondStartHoursToMinutes = secondStartHours * 60;
+        secondStartMinutes = secondIntervalStart % 100;
+        secondStartTotalMinutes = secondStartHoursToMinutes 
+            + secondStartMinutes;
+
+        // Convert Second Time End to Minutes
+        secondEndHours = secondIntervalEnd / 100;
+        secondEndHoursToMinutes = secondEndHours * 60;
+        secondEndMinutes = secondIntervalEnd % 100;
+        secondEndTotalMinutes = secondEndHoursToMinutes 
+            + secondEndMinutes;
+
+        // Second Interval Total Time in Minutes
+        secondTotalTime = secondEndTotalMinutes 
+                - secondStartTotalMinutes;
+
+        // Output Interval Times
         System.out.println("The earlier interval is " + firstTotalTime + 
             " minutes long.");
-        
-        // Calculate Second Time Interval
-        secondTotalTime = secondIntervalEnd - secondIntervalStart;
         System.out.println("The later interval is " + secondTotalTime + 
             " minutes long.");
         
         // Compare total times print which interval is longer
         if (firstTotalTime == secondTotalTime) {
-            System.out.println("Both intervals are the same duration");
+            System.out.println("Both intervals are equally long.");
         }
         else if(firstTotalTime > secondTotalTime) {
-            System.out.println("The first interval is longer");
+            System.out.println("The eariler interval is longer.");
         }
         else {
-            System.out.println("The second interval is longer");
+            System.out.println("The later interval is longer.");
         }
 
         // Determine if the intervals overlap

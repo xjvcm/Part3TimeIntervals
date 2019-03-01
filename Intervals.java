e233fb2d495ef4fe8d2fd00944b88efb56b20e1e
Yanni App13 <jonvincruman@gmail.com>
2019-02-28 16:11:17 -0800

    added validations

--- Intervals.java
+++ Intervals.java
@@ -14,7 +14,11 @@ import java.util.Scanner;
 
 public class Intervals {
     public static void main(String[] args) {
+        // Declare constant
+        final int MINS_IN_HOUR = 60;
+
         // Declare variables
+        int x1, x2, y1, y2;
         int firstIntervalStart, firstIntervalEnd, secondIntervalStart;
         int secondIntervalEnd, firstTotalTime, secondTotalTime;
         int firstStartHours, firstStartMinutes, firstStartTotalMinutes;
@@ -30,65 +34,134 @@ public class Intervals {
 
         // Prompt User to input First Time Interval
         System.out.print("Enter earlier start and end time as two 24-hour "
-            + "format times: ");
-        firstIntervalStart = input.nextInt();
-        firstIntervalEnd = input.nextInt();
+            + "format times(XXXX XXXX *Use SPACE to seperate times*): ");
+        x1 = input.nextInt();
+        x2 = input.nextInt();
 
+        // Valid if input is within 0000 - 2400
+        while (x1 < 0 || x1 > 2400) {
+            System.out.print(x1 + " is an invalid start time.  Please " + 
+                "time within 0000 - 2400: ");
+            x1 = input.nextInt();
+            
+            // Debug
+            System.out.println(x1 + " is valid");
+        }
+
+        // Valid if input is within 0000 - 2400
+        while (x2 < 0 || x2 > 2400) {
+            System.out.print(x2 + " is an invalid end time.  Please " + 
+                "time within 0000 - 2400: ");
+            x2 = input.nextInt();
+            
+            // Debug
+            System.out.println(x2 + " is valid");
+        }
+        
+        // Prompt User to InputSecond Time Interval
+        System.out.print("Enter later start and end time as two 24-hour "
+        + "format times(XXXX XXXX *Use SPACE to seperate times*): ");
+        y1 = input.nextInt();
+        y2 = input.nextInt();
+
+        // Valid if input is within 0000 - 2400
+        while (y1 < 0 || y1 > 2400) {
+            System.out.print(y1 + " is an invalid start time.  Please " + 
+                "time within 0000 - 2400: ");
+            y1 = input.nextInt();
+            
+            // Debug
+            System.out.println(y1 + " is valid");
+        }
+
+        // // Valid if input is within 0000 - 2400
+        while (y2 < 0 || y2 > 2400) {
+            System.out.print(y2 + " is an invalid end time.  Please " + 
+                "time within 0000 - 2400: ");
+            y2 = input.nextInt();
+            
+            // Debug
+            System.out.println(y2 + " is valid");
+        }
+        
+        
         // Validate first interval inputs and swap if necessary
-        if (firstIntervalStart > firstIntervalEnd) {
-            temp = firstIntervalStart;
-            firstIntervalStart = firstIntervalEnd;
-            firstIntervalEnd = temp;
+        if (x1 > x2) {
+            temp = x1;
+            x1 = x2;
+            x2 = temp;
 
             // Debugging
+            System.out.println(x1 + " this is first start");
+            System.out.println(x2 + " this is first end");
+        }
+        
+        // Validate second interval inputs and swap if necessary
+        if (y1 > y2) {
+            temp = y1;
+            y1 =  y2;
+            y2 = temp;
+            
+            // Debugging
+            System.out.println(y1 + " this is second start");
+            System.out.println(y2 + " this is second end");
+            
+        }
+        
+        // Check and validate if they user inputed the time intervals in 
+        // correct order
+        if (x1 > y1) {
+            secondIntervalStart = x1;
+            secondIntervalEnd = x2;
+            firstIntervalStart = y1;
+            firstIntervalEnd = y2;
+
+            // Debug
+            System.out.println(firstIntervalStart + " this is first start");
+            System.out.println(firstIntervalEnd + " this is first end");
+            System.out.println(secondIntervalStart + " this is second start");
+            System.out.println(secondIntervalEnd + " this is second end");
+        } else {
+            firstIntervalStart = x1;
+            firstIntervalEnd = x2;
+            secondIntervalStart = y1;
+            secondIntervalEnd = y2;
+
+            // Debug
             System.out.println(firstIntervalStart);
             System.out.println(firstIntervalEnd);
+            System.out.println(secondIntervalStart);
+            System.out.println(secondIntervalEnd);
         }
+
+
 
         // Convert First Interval Start to Minutes
         firstStartHours = firstIntervalStart / 100;
-        firstStartHoursToMinutes = firstStartHours * 60;
+        firstStartHoursToMinutes = firstStartHours * MINS_IN_HOUR;
         firstStartMinutes = firstIntervalStart % 100;
         firstStartTotalMinutes = firstStartHoursToMinutes 
             + firstStartMinutes;
-
+        
         // Convert First Interval End Minutes
         firstEndHours = firstIntervalEnd / 100;
-        firstEndHoursToMinutes = firstEndHours * 60;
+        firstEndHoursToMinutes = firstEndHours * MINS_IN_HOUR;
         firstEndMinutes = firstIntervalEnd % 100;
         firstEndTotalMinutes = firstEndHoursToMinutes + firstEndMinutes;
-
+        
         // First Interval Total Time in Minutes
         firstTotalTime = firstEndTotalMinutes - firstStartTotalMinutes;
 
-        // Prompt User to InputSecond Time Interval
-        System.out.print("Enter later start and end time as two 24-hour "
-            + "format times: ");
-        secondIntervalStart = input.nextInt();
-        secondIntervalEnd = input.nextInt();
-
-        // Validate second interval inputs and swap if necessary
-        if (secondIntervalStart > secondIntervalEnd) {
-            temp = secondIntervalStart;
-            secondIntervalStart =  secondIntervalEnd;
-            secondIntervalEnd = temp;
-
-            // Debugging
-            System.out.println(secondIntervalStart);
-            System.out.println(secondIntervalEnd);
-
-        }
-
         // Convert Second Time Start to Minutes
         secondStartHours = secondIntervalStart / 100;
-        secondStartHoursToMinutes = secondStartHours * 60;
+        secondStartHoursToMinutes = secondStartHours * MINS_IN_HOUR;
         secondStartMinutes = secondIntervalStart % 100;
         secondStartTotalMinutes = secondStartHoursToMinutes 
             + secondStartMinutes;
 
         // Convert Second Time End to Minutes
         secondEndHours = secondIntervalEnd / 100;
-        secondEndHoursToMinutes = secondEndHours * 60;
+        secondEndHoursToMinutes = secondEndHours * MINS_IN_HOUR;
         secondEndMinutes = secondIntervalEnd % 100;
         secondEndTotalMinutes = secondEndHoursToMinutes 
             + secondEndMinutes;
